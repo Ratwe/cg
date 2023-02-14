29a6ff22e77a726e2cec6d47f9076f52ac42efeb
Artem Dyachenko <artemon1324@yandex.ru>
2023-02-14 21:22:19 +0300

    Щас буду переписывать все эти графики

--- interface.py
+++ interface.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import random
+from copy import copy
 
 from numpy import sqrt
 
@@ -7,7 +8,8 @@ from main import get_min_difference, Point, get_circle_center, get_circle_radius
 
 canvas_width = 600
 canvas_height = 600
-coords_range = min(canvas_height, canvas_width) / 4
+coords_range = 1000  # min(canvas_height, canvas_width)
+padding = 0
 
 points = []
 triangle = []
@@ -15,8 +17,8 @@ circle = []
 o_text = []
 o_centre = []
 r = canvas_width / 2
-width = 15
-pnum = 1
+tk_width = 15
+pnum = 4
 
 
 def display_message(message, color):
@@ -69,7 +71,9 @@ def delete_point():
 
     if selected:
         index = selected[0]
-        display_message(f"Point {points[index].num} ({- canvas_width / 2 + points[index].x}, {canvas_height / 2 - points[index].y}) removed", "green")
+        display_message(
+            f"Point {points[index].num} ({- canvas_width / 2 + points[index].x}, {canvas_height / 2 - points[index].y}) removed",
+            "green")
         listbox.delete(index)
         del points[index]
         canvas.delete("all")
@@ -92,6 +96,33 @@ def solve():
     p2 = points[res_points[1]]
     p3 = points[res_points[2]]
 
+    for line in triangle:
+        canvas.delete(line)
+    for auto in triangle:
+        canvas.delete(auto)
+
+    # p1_coords = Point(- canvas_width / 2 + p1.x, canvas_height / 2 - p1.y, None)
+    # triangle.append(
+    #     canvas.create_text(p1.x + 10, p1.y - 10, text=f"({p1_coords.x:.3f}, {p1_coords.y:.3f})", anchor=tk.W))
+    # triangle.append(canvas.create_text(p1.x - 10, p1.y + 12, text=f"№{p1.num}", anchor=tk.W))
+    #
+    # p2_coords = Point(- canvas_width / 2 + p2.x, canvas_height / 2 - p2.y, None)
+    # triangle.append(
+    #     canvas.create_text(p2.x + 10, p2.y - 10, text=f"({p2_coords.x:.3f}, {p2_coords.y:.3f})", anchor=tk.W))
+    # triangle.append(canvas.create_text(p2.x - 10, p2.y + 12, text=f"№{p2.num}", anchor=tk.W))
+    #
+    # p3_coords = Point(- canvas_width / 2 + p3.x, canvas_height / 2 - p3.y, None)
+    # triangle.append(
+    #     canvas.create_text(p3.x + 10, p3.y - 10, text=f"({p3_coords.x:.3f}, {p3_coords.y:.3f})", anchor=tk.W))
+    # triangle.append(canvas.create_text(p3.x - 10, p3.y + 12, text=f"№{p3.num}", anchor=tk.W))
+    #
+    # line1 = canvas.create_line(p1.x, p1.y, p2.x, p2.y, fill="blue")
+    # line2 = canvas.create_line(p2.x, p2.y, p3.x, p3.y, fill="blue")
+    # line3 = canvas.create_line(p3.x, p3.y, p1.x, p1.y, fill="blue")
+    # triangle.append(line1)
+    # triangle.append(line2)
+    # triangle.append(line3)
+    #
     o = get_circle_center(p1, p2, p3)
     if o is None:
         display_message("Error: Impossible to get circle centre", "red")
@@ -99,44 +130,19 @@ def solve():
             canvas.delete(auto)
         return
     r = get_circle_radius(p1, p2, p3)
-    real_r = 250
-    k = r / real_r
-
-    for auto in triangle:
-        canvas.delete(auto)
-
-    triangle.append(canvas.create_oval(p1.x * k - 3, p1.y * k - 3,
-                                       p1.x * k + 3, p1.y * k + 3, fill="black"))
-    triangle.append(canvas.create_oval(p2.x - 3, p2.y - 3, p2.x + 3, p2.y + 3, fill="black"))
-    triangle.append(canvas.create_oval(p3.x - 3, p3.y - 3, p3.x + 3, p3.y + 3, fill="black"))
-
-    p1_coords = Point(- canvas_width / 2 + p1.x, canvas_height / 2 - p1.y, None)
-    triangle.append(canvas.create_text(p1.x + 10, p1.y - 10, text=f"({p1_coords.x:.3f}, {p1_coords.y:.3f})", anchor=tk.W))
-    triangle.append(canvas.create_text(p1.x - 10, p1.y + 12, text=f"№{p1.num}", anchor=tk.W))
-
-    p2_coords = Point(- canvas_width / 2 + p2.x, canvas_height / 2 - p2.y, None)
-    triangle.append(canvas.create_text(p2.x + 10, p2.y - 10, text=f"({p2_coords.x:.3f}, {p2_coords.y:.3f})", anchor=tk.W))
-    triangle.append(canvas.create_text(p2.x - 10, p2.y + 12, text=f"№{p2.num}", anchor=tk.W))
-
-    p3_coords = Point(- canvas_width / 2 + p3.x, canvas_height / 2 - p3.y, None)
-    triangle.append(canvas.create_text(p3.x + 10, p3.y - 10, text=f"({p3_coords.x:.3f}, {p3_coords.y:.3f})", anchor=tk.W))
-    triangle.append(canvas.create_text(p3.x - 10, p3.y + 12, text=f"№{p3.num}", anchor=tk.W))
-
-    line1 = canvas.create_line(p1.x, p1.y, p2.x, p2.y, fill="blue")
-    line2 = canvas.create_line(p2.x, p2.y, p3.x, p3.y, fill="blue")
-    line3 = canvas.create_line(p3.x, p3.y, p1.x, p1.y, fill="blue")
-    triangle.append(line1)
-    triangle.append(line2)
-    triangle.append(line3)
 
     for auto in circle:
         canvas.delete(auto)
-    circle.append(canvas.create_oval(canvas_width / 2 - real_r, canvas_height / 2 - real_r, canvas_width / 2 + real_r,
-                                     canvas_height / 2 + real_r, fill='', outline='green'))
+    circle.clear()
+    # circle.append(canvas.create_oval(o.x - r, o.y - r, o.x + r, o.y + r, fill='', outline='green'))
+    #
+    # circle.append(canvas.create_oval(o.x - 3, o.y - 3, o.x + 3, o.y + 3, fill="green"))
+    # o_coords = Point(- canvas_width / 2 + o.x, canvas_height / 2 - o.y, None)
+    # circle.append(canvas.create_oval(o.x - 4, o.y - 4, o.x + 4, o.y + 4, fill="green"))
+    # circle.append(canvas.create_text(o.x + 10, o.y - 10, text=f"({o_coords.x:.3f}, {o_coords.y:.3f})", anchor=tk.W))
 
-    o_coords = Point(- canvas_width / 2 + o.x, canvas_height / 2 - o.y, None)
-    circle.append(canvas.create_oval(canvas_width / 2 - 4, canvas_height / 2 - 4, canvas_width / 2 + 4, canvas_height / 2 + 4, fill="green"))
-    circle.append(canvas.create_text(canvas_width / 2 + 10, canvas_height / 2 - 10, text=f"({o_coords.x:.3f}, {o_coords.y:.3f})", anchor=tk.W))
+    scale_to_fit_bbox(p1, p2, p3, o)
+    # draw_triangle(p1, p2, p3, o)
 
     display_message(f"Result: minimal diff is {min_diff}: points in triangle - {p_in}, out of - {p_out}\n"
                     f"Triangle is based on points №{p1.num}, {p2.num}, {p3.num}", "black")
@@ -155,32 +161,141 @@ def draw_canvas():
     # Draw the origin
     # canvas.create_text(canvas_width / 2 + 5, canvas_height / 2 + 10, text="(0, 0)", anchor=tk.W)
 
-    # Draw the x and y axis ticks
-    for x in range(0, canvas_width - 1, 50):
-        if not x or abs(x) == canvas_width:
-            continue
-        canvas.create_line(x, canvas_height - 5, x, canvas_height, width=1)
-        canvas.create_text(x, canvas_height - 20, text=str(x - canvas_width / 2), anchor='n')
+    # # Draw the x and y axis ticks
+    # for x in range(0, canvas_width - 1, 50):
+    #     if not x or abs(x) == canvas_width:
+    #         continue
+    #     canvas.create_line(x, canvas_height - 5, x, canvas_height, width=1)
+    #     canvas.create_text(x, canvas_height - 20, text=str(x - canvas_width / 2), anchor='n')
+    #
+    # for y in range(0, canvas_height - 1, 50):
+    #     if not y or abs(y) == canvas_height:
+    #         continue
+    #     canvas.create_line(-5, y, 5, y, width=1)
+    #     canvas.create_text(10, y, text=str(canvas_height / 2 - y), anchor='w')
 
-    for y in range(0, canvas_height - 1, 50):
-        if not y or abs(y) == canvas_height:
-            continue
-        canvas.create_line(-5, y, 5, y, width=1)
-        canvas.create_text(10, y, text=str(canvas_height / 2 - y), anchor='w')
-
-    # Label the x and y axis
-    canvas.create_text(canvas_width - 3, canvas_height - 15, text='X', anchor='e')
-    canvas.create_text(15, 10, text='Y', anchor='w')
-
-    # draw X axis
-    canvas.create_line(3, canvas_height - 3, canvas_width, canvas_height - 3, arrow=tk.LAST, fill='black')
-
-    # draw Y axis
-    canvas.create_line(5, canvas_height, 5, 5, arrow=tk.LAST, fill='black')
+    # # Label the x and y axis
+    # canvas.create_text(canvas_width - 3, canvas_height - 15, text='X', anchor='e')
+    # canvas.create_text(15, 10, text='Y', anchor='w')
+    #
+    # # draw X axis
+    # canvas.create_line(3, canvas_height - 3, canvas_width, canvas_height - 3, arrow=tk.LAST, fill='black')
+    #
+    # # draw Y axis
+    # canvas.create_line(5, canvas_height, 5, 5, arrow=tk.LAST, fill='black')
 
     # draw black border around canvas
     # canvas.create_rectangle(2, 2, canvas_width, canvas_height, outline='black')
 
+
+def scale_to_fit_bbox(a, b, c, center):
+    # определяем bbox
+    rad = get_circle_radius(a, b, c)
+    bbox = (center.x - rad, center.y - rad, center.x + rad, center.y + rad)
+
+    # получаем размеры bbox
+    bbox_width = canvas_width - padding * 2
+    bbox_height = canvas_height - padding * 2
+
+    # определяем масштаб, необходимый для изменения размера графика
+    scale = min(canvas_width / bbox_width, canvas_height / bbox_height)
+
+    # определяем координаты центра bbox
+    center_x = center.x
+    center_y = center.y
+
+    # применяем масштабирование и перемещение координат
+    ap = copy(a)
+    bp = copy(b)
+    cp = copy(c)
+    cc = copy(center)
+
+    for point in [ap, bp, cp, cc]:
+        point.x = (point.x - center_x) * scale + canvas_width / 2
+        point.y = (point.y - center_y) * scale + canvas_height / 2
+
+    # проверяем, выходит ли окружность за пределы графика
+    max_radius = min(canvas_width, canvas_height) / 2
+    if get_circle_radius(ap, bp, cp) > max_radius:
+        # масштабируем график так, чтобы радиус окружности был равен максимально возможному значению в пределах графика
+        scale = max_radius / get_circle_radius(ap, bp, cp)
+
+    # перерисовываем график
+    circle.append(canvas.create_oval(cc.x - scale * get_circle_radius(ap, bp, cp),
+                                     cc.y - scale * get_circle_radius(ap, bp, cp),
+                                     cc.x + scale * get_circle_radius(ap, bp, cp),
+                                     cc.y + scale * get_circle_radius(ap, bp, cp),
+                                     outline="green"))
+
+    circle.append(canvas.create_line(scale * ap.x, scale * ap.y, scale * bp.x, scale * bp.y))
+    circle.append(canvas.create_line(scale * bp.x, scale * bp.y, scale * cp.x, scale * cp.y))
+    circle.append(canvas.create_line(scale * cp.x, cp.y, ap.x, ap.y))
+    circle.append(canvas.create_oval(ap.x - 3, ap.y - 3, ap.x + 3, ap.y + 3, fill="green"))
+    circle.append(canvas.create_oval(bp.x - 3, bp.y - 3, bp.x + 3, bp.y + 3, fill="green"))
+    circle.append(canvas.create_oval(cp.x - 3, cp.y - 3, cp.x + 3, cp.y + 3, fill="green"))
+    circle.append(canvas.create_oval(cc.x - 3, cc.y - 3, cc.x + 3, cc.y + 3, fill="blue"))
+
+    o_coords = Point(- canvas_width / 2 + cc.x, canvas_height / 2 - cc.y, None)
+    circle.append(canvas.create_oval(cc.x - 4, cc.y - 4, cc.x + 4, cc.y + 4, fill="green"))
+    circle.append(canvas.create_text(cc.x + 10, cc.y - 10, text=f"({center.x:.3f}, {center.y:.3f})", anchor=tk.W))
+
+    canvas.create_rectangle(bbox[0], bbox[1], bbox[2], bbox[3],
+                            outline='red')
+
+
+def draw_triangle(a, b, c, center):
+    # определяем bbox
+    rad = get_circle_radius(a, b, c)
+    bbox = (0, 0, center.x + rad, center.y + rad)
+
+    # получаем размеры bbox
+    bbox_width = bbox[2]
+    bbox_height = bbox[3]
+
+    # определяем координаты центра bbox
+    bbox_center_x = bbox_width / 2
+    bbox_center_y = bbox_height / 2
+
+    # определяем размеры и координаты центра окружности относительно bbox
+    circle_center_x = center.x - bbox[0]
+    circle_center_y = center.y - bbox[1]
+    circle_radius = get_circle_radius(a, b, c)
+
+    # определяем размеры и координаты вершин треугольника относительно bbox
+    triangle_points = [a, b, c]
+    triangle_points_x = [(p.x - bbox[0]) for p in triangle_points]
+    triangle_points_y = [(p.y - bbox[1]) for p in triangle_points]
+
+    # определяем масштаб, необходимый для изменения размера окружности и треугольника
+    scale = min(canvas_width / bbox_width, canvas_height / bbox_height)
+
+    # применяем масштаб и сдвиг координат, чтобы переместить окружность и треугольник в центр графика
+    circle_center_x = circle_center_x * scale + (canvas_width / 2 - bbox_center_x * scale)
+    circle_center_y = circle_center_y * scale + (canvas_height / 2 - bbox_center_y * scale)
+    circle_radius *= scale
+    triangle_points_x = [x * scale + (canvas_width / 2 - bbox_center_x * scale) for x in triangle_points_x]
+    triangle_points_y = [y * scale + (canvas_height / 2 - bbox_center_y * scale) for y in triangle_points_y]
+
+    # перерисовываем окружность
+    rad = get_circle_radius(a, b, c)
+    circle_center = center
+    if circle_center.x - rad < 0:
+        circle_center.x = rad
+    if circle_center.y - rad < 0:
+        circle_center.y = rad
+    if circle_center.x + rad > bbox[2]:
+        circle_center.x = bbox[2] - rad
+    if circle_center.y + rad > bbox[3]:
+        circle_center.y = bbox[3] - rad
+
+    circle.append(canvas.create_oval(circle_center.x - rad,
+                                     circle_center.y - rad,
+                                     circle_center.x + rad,
+                                     circle_center.y + rad,
+                                     outline="green"))
+
+    canvas.create_rectangle(bbox[0], bbox[1], bbox[2], bbox[3],
+                            outline='red')
 
 root = tk.Tk()
 root.title("Задача о нахождении оптимального треугольника")
@@ -194,7 +309,7 @@ message_box.pack(side='top', fill='x', padx=5, pady=5)
 label = tk.Label(root, text="На плоскости дано множество точек.\nНайти такой треугольник с вершинами в этих точках, "
                             "для которого разность количеств\nточек этого множества, попавших внутрь треугольника и за "
                             "его пределы, но внутри описанной окружности, минимальна.", font=("Arial", 12),
-                            wraplength=root.winfo_screenwidth(), justify="left")
+                 wraplength=root.winfo_screenwidth(), justify="left")
 
 # располагаем метку внизу окна и центрируем ее по левой стороне
 label.pack(side="bottom", anchor="w")
@@ -214,24 +329,33 @@ x_label = tk.Label(root, text="Enter X:")
 x_label.pack(side='top')
 
 x_entry = tk.Entry(root)
-x_entry.pack(side='top', padx=width - 1, pady=5)
+x_entry.pack(side='top', padx=tk_width - 1, pady=5)
 
 y_label = tk.Label(root, text="Enter Y:")
 y_label.pack(side='top')
 
 y_entry = tk.Entry(root)
-y_entry.pack(side='top', padx=width - 1, pady=5)
+y_entry.pack(side='top', padx=tk_width - 1, pady=5)
 
-add_button = tk.Button(root, text="Add", command=add_point, width=width)
+add_button = tk.Button(root, text="Add", command=add_point, width=tk_width)
 add_button.pack(side='top')
 
-add_random_button = tk.Button(root, text="Add Random Point", command=add_random_point, width=width)
+add_random_button = tk.Button(root, text="Add Random Point", command=add_random_point, width=tk_width)
 add_random_button.pack(side='top')
 
-delete_button = tk.Button(root, text="Delete Point", command=delete_point, width=width)
+delete_button = tk.Button(root, text="Delete Point", command=delete_point, width=tk_width)
 delete_button.pack(side='top')
 
-solve_button = tk.Button(root, text="Solve", command=solve, width=width)
+solve_button = tk.Button(root, text="Solve", command=solve, width=tk_width)
 solve_button.pack(side='top')
+
+points.append(Point(0, 100, 1))
+listbox.insert(tk.END, f"Point {1}: ({0}, {100})")
+
+points.append(Point(100, 500, 2))
+listbox.insert(tk.END, f"Point {2}: ({100}, {500})")
+
+points.append(Point(100, -50, 3))
+listbox.insert(tk.END, f"Point {3}: ({100}, {-50})")
 
 root.mainloop()
