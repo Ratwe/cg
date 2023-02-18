1fa9b8233a0167d3873543e3b408bcd05dac6e53
Artem Dyachenko <artemon1324@yandex.ru>
2023-02-18 08:57:58 +0300

    Добавил кучу комментариев

--- interface.py
+++ interface.py
@@ -19,35 +19,36 @@ GRAPH_HEIGHT = 60
 TEXT_COLOR = "LightSkyBlue"
 
 # Cicloid
-NUMB_POINTS = 300
-A_KOEF = 2
-B_KOEF = 3
+CYCLOID_POINTS = 300  # количество точек для построения эпициклоиды
+# должны быть в отношении 2 / 3
+A_KF = 2
+B_KF = 3
 
 # Rectangle
-LEFT_UP_X = -18
-LEFT_UP_Y = 10
-RIGHT_DOWN_X = 18
-RIGHT_DOWN_Y = -10
+LEFT_UP_X = -18  # координата X левой верхней точки прямоугольника
+LEFT_UP_Y = 10  # координата Y левой верхней точки прямоугольника
+RIGHT_DOWN_X = 18  # координата X правой нижней точки прямоугольника
+RIGHT_DOWN_Y = -10  # координата Y правой нижней точки прямоугольника
 
 EPS = 0.14  # 0.1973
 
-X_CENTER = 0
-Y_CENTER = 0
+X_CENTER = 0  # центр по оси X
+Y_CENTER = 0  # центр по оси Y
 
 
-def cicloid(t):
+def cycloid(f):
     """
-        Функция для получения координат точки циклоиды при параметре t
+        Функция для получения координат точки эпициклоиды при параметре f
     """
-    x = (A_KOEF + B_KOEF) * np.cos(t) - A_KOEF * np.cos((A_KOEF + B_KOEF) * t / A_KOEF)
-    y = (A_KOEF + B_KOEF) * np.sin(t) - A_KOEF * np.sin((A_KOEF + B_KOEF) * t / A_KOEF)
+    x = (A_KF + B_KF) * np.cos(f) - A_KF * np.cos((A_KF + B_KF) * f / A_KF)
+    y = (A_KF + B_KF) * np.sin(f) - A_KF * np.sin((A_KF + B_KF) * f / A_KF)
 
     return x + X_CENTER, y + Y_CENTER
 
 
 def check_intersection(x_graph, y_graph, x, y):
     """
-       Функция для нахождения пересечения между линией штриховки прямоугольника и циклоидой
+       Функция для нахождения пересечения между линией штриховки прямоугольника и эпициклоидой
     """
     for i in range(len(x_graph)):
         if (abs(x_graph[i] - x) < EPS) and (abs(y_graph[i] - y) < EPS):
@@ -64,6 +65,7 @@ def init_lines_rect(x_graph, y_graph):
     x_all = []
     y_all = []
 
+    # Цикл, в котором проходятся все значения x, чтобы нарисовать линии на прямоугольнике
     for i in range(LEFT_UP_X + X_CENTER, RIGHT_DOWN_X + X_CENTER, 5):
         y_b = RIGHT_DOWN_Y + Y_CENTER
         y_e = LEFT_UP_Y + Y_CENTER
@@ -74,12 +76,13 @@ def init_lines_rect(x_graph, y_graph):
         x_line = []
         y_line = []
 
-        while (y_b <= y_e):
+        # Цикл, в котором проходимся по всем y от y_b до y_e и добавляем координаты в x_line и y_line
+        while y_b <= y_e:
 
-            if (check_intersection(x_graph, y_graph, x_b, y_b) == 1):
+            if check_intersection(x_graph, y_graph, x_b, y_b) == 1:
                 break
 
-            if (x_b < x_e):
+            if x_b < x_e:
                 break
 
             x_line.append(x_b)
@@ -101,12 +104,12 @@ def init_lines_rect(x_graph, y_graph):
         x_line = []
         y_line = []
 
-        while (y_b >= y_e):
+        while y_b >= y_e:
 
-            if (check_intersection(x_graph, y_graph, x_b, y_b) == 1):
+            if check_intersection(x_graph, y_graph, x_b, y_b) == 1:
                 break
 
-            if (x_b > x_e):
+            if x_b > x_e:  # Если x начала линии больше x конца, то выходим из цикла
                 break
 
             x_line.append(x_b)
@@ -120,44 +123,41 @@ def init_lines_rect(x_graph, y_graph):
     return x_all, y_all
 
 
+# Функция для отрисовки линий штриховки на прямоугольнике
 def draw_lines_rect(x_lines, y_lines):
-    '''
-       Функция для отрисовки линий штриховки
-    '''
     for line in range(3, len(x_lines)):
-        plt.plot(x_lines[line], y_lines[line], 'b')
+        plt.plot(x_lines[line], y_lines[line], 'b')  # Рисуем линию по координатам из x_lines и y_lines
 
 
+# Функция для нахождения координат вершин прямоугольника
 def init_rectangle():
-    '''
-        Функция для нахождения координат вершин прямоугольника
-    '''
     x_rect = [LEFT_UP_X + X_CENTER, RIGHT_DOWN_X + X_CENTER, RIGHT_DOWN_X + X_CENTER, LEFT_UP_X + X_CENTER,
-              LEFT_UP_X + X_CENTER]
+              LEFT_UP_X + X_CENTER]  # Задаем координаты x вершин прямоугольника
     y_rect = [RIGHT_DOWN_Y + Y_CENTER, RIGHT_DOWN_Y + Y_CENTER, LEFT_UP_Y + Y_CENTER, LEFT_UP_Y + Y_CENTER,
-              RIGHT_DOWN_Y + Y_CENTER]
+              RIGHT_DOWN_Y + Y_CENTER]  # Задаем координаты y вершин прямоугольника
 
-    return x_rect, y_rect
+    return x_rect, y_rect  # Возвращаем координаты вершин прямоугольника
 
 
 def draw_rectangle(x_rect, y_rect):
-    '''
-       Функция для отрисовки прямоугльника
-    '''
+    """
+       Функция для отрисовки прямоугольника
+    """
     plt.plot(x_rect, y_rect, linewidth=3)
 
 
-def init_graph():
-    '''
-        Функция для вычисления координат циклоиды для ее последующего построения
-    '''
+# Получаем координаты точек эпициклоиды
+def init_cycloid():
+    """
+        Функция для вычисления координат эпициклоиды для ее последующего построения
+    """
     x_arr = []
     y_arr = []
 
-    arr = np.linspace(0, 4 * pi, NUMB_POINTS)
+    arr = np.linspace(0, 4 * pi, CYCLOID_POINTS)
 
     for i in arr:
-        x, y = cicloid(i)
+        x, y = cycloid(i)
 
         x_arr.append(x)
         y_arr.append(y)
@@ -166,30 +166,33 @@ def init_graph():
 
 
 def draw_graph(x_graph, y_graph):
-    '''
-        Функция для отрисовки графика циклоиды
-    '''
+    """
+        Функция для отрисовки графика эпициклоиды
+    """
     plt.plot(x_graph, y_graph, linewidth=3)
 
 
 def draw_picrure(x_all, y_all):
-    '''
+    """
         Функция для отрисовки всей картины
-    '''
+    """
     build_empty_figure()
 
+    # Рисуем линии на прямоугольнике
     draw_lines_rect(x_all, y_all)
+    # Рисуем прямоугольник
     draw_rectangle(x_all[2], y_all[2])
+    # Рисуем эпициклоиду
     draw_graph(x_all[1], y_all[1])
 
     canvas.draw()
 
 
 def init_all():
-    '''
+    """
         Функция для вычисления координат всех необходимых объектов
-    '''
-    x_graph, y_graph = init_graph()
+    """
+    x_graph, y_graph = init_cycloid()
     x_lines, y_lines = init_lines_rect(x_graph, y_graph)
     x_rect, y_rect = init_rectangle()
 
@@ -206,10 +209,11 @@ def init_all():
 
 
 def move(x_all, y_all, dx, dy):
-    '''
+    """
         Функция для вычисления координат всех нужных точек при перемещении
-    '''
+    """
 
+    # x_all = [[X_CENTER], x_graph, x_rect]
     for i in range(len(x_all)):
         for j in range(len(x_all[i])):
             x_all[i][j] += dx
@@ -217,16 +221,18 @@ def move(x_all, y_all, dx, dy):
 
     set_figure_center(x_all[0][0], y_all[0][0])
 
+    # deepcopy по причине copy не копирует элементы массива
     x_history.append(copy.deepcopy(x_all))
     y_history.append(copy.deepcopy(y_all))
 
+    # отрисовать новейшие фигуры
     draw_picrure(x_history[len(x_history) - 1], y_history[len(y_history) - 1])
 
 
 def spin(x_all, y_all, angle, x_c, y_c):
-    '''
+    """
         Функция для вычисления координат всех нужных точек при повороте
-    '''
+    """
 
     angle = (angle * pi) / 180
 
@@ -234,11 +240,13 @@ def spin(x_all, y_all, angle, x_c, y_c):
 
     for i in range(len(x_all)):
         for j in range(len(x_all[i])):
+            # чтобы координаты центра не мешали вычислениям
             x_all[i][j] -= x_c
             y_all[i][j] -= y_c
 
             coords = np.dot(matrix_spin, [x_all[i][j], y_all[i][j]])
 
+            # возвращаем координаты центра
             x_all[i][j] = coords[0] + x_c
             y_all[i][j] = coords[1] + y_c
 
@@ -251,12 +259,13 @@ def spin(x_all, y_all, angle, x_c, y_c):
 
 
 def scale(x_all, y_all, x_c, y_c, kx, ky):
-    '''
+    """
         Функция для вычисления координат всех нужных точек при масштабировании
-    '''
+    """
 
     for i in range(len(x_all)):
         for j in range(len(x_all[i])):
+            # чтобы координаты центра не мешали вычислениям
             x_all[i][j] = kx * (x_all[i][j] - x_c) + x_c
             y_all[i][j] = ky * (y_all[i][j] - y_c) + y_c
 
@@ -269,28 +278,29 @@ def scale(x_all, y_all, x_c, y_c, kx, ky):
 
 
 def build_empty_figure():
-    '''
+    """
         Функция для очистки поля для последующего построения на нем фигуры
-    '''
+    """
     global ax
 
     fig.clear()
 
+    # 1x1, первый subplot
     ax = fig.add_subplot(111)
 
     ax.set_xlim([-GRAPH_WIDE, GRAPH_WIDE])
     ax.set_ylim([-GRAPH_HEIGHT, GRAPH_HEIGHT])
     ax.grid()
 
-    fig.subplots_adjust(right=0.97, left=0.06, bottom=0.06, top=0.97)
+    fig.subplots_adjust(right=0.95, left=0.05, bottom=0.05, top=0.95)
 
     canvas.draw()
 
 
 def reset():
-    '''
+    """
         Функция для сброса всех преобразований и возврата к начальному состояни фигуры
-    '''
+    """
     global x_all, y_all, x_history, y_history
 
     set_figure_center(0, 0)
@@ -305,9 +315,9 @@ def reset():
 
 
 def step_backing():
-    '''
+    """
         Функция для возврата к предыдущему состоянию фигуры
-    '''
+    """
 
     if (len(x_history) == 1):
         messagebox.showerror("Стоп", "Вы дошли до начального изображения")
@@ -322,9 +332,9 @@ def step_backing():
 
 
 def parse_move():
-    '''
+    """
         Функция обработки параметров для перемещения и вызова функции перемещения
-    '''
+    """
     try:
         dx = float(move_x.get())
         dy = float(move_y.get())
@@ -339,9 +349,9 @@ def parse_move():
 
 
 def parse_spin():
-    '''
+    """
         Функция обработки параметров для поворота и вызова функции поворота
-    '''
+    """
     try:
         x_c = float(center_x.get())
         y_c = float(center_y.get())
@@ -360,19 +370,19 @@ def parse_spin():
 
 
 def set_figure_center(x_c, y_c):
-    '''
+    """
         Функция для отрисовки в приложении текущего центра фигуры
-    '''
+    """
 
-    figure_c = Label(win, text="Центр фигуры: (%3.2f;%3.2f)" % (x_c, y_c), width=32, font=("Arial", 14),
+    figure_c = Label(root, text="Центр фигуры: (%3.2f;%3.2f)" % (x_c, y_c), width=32, font=("Arial", 14),
                      bg=TEXT_COLOR)
     figure_c.place(x=CV_WIDE + 1, y=850)
 
 
 def parse_scale():
-    '''
+    """
         Функция обработки параметров для масштабирования и вызова функции масштабирования
-    '''
+    """
     try:
         x_c = float(center_x.get())
         y_c = float(center_y.get())
@@ -391,18 +401,18 @@ def parse_scale():
     scale(x_cur, y_cur, x_c, y_c, kx, ky)
 
 
-win = Tk()
-win['bg'] = WIN_COLOR
-win.geometry("%dx%d" % (WIN_WIDTH, WIN_HEIGHT))
-win.title("Лабораторная работа #2 (Цветков И.А. ИУ7-43Б)")
-win.resizable(False, False)
+root = Tk()
+root['bg'] = WIN_COLOR
+root.geometry(f"{WIN_WIDTH}x{WIN_HEIGHT}")
+root.title("Лабораторная работа №2")
+root.resizable(False, False)
 
 x_history = []
 y_history = []
 
 fig = plt.figure()
 
-canvas = FigureCanvasTkAgg(fig, master=win)
+canvas = FigureCanvasTkAgg(fig, master=root)
 plot = canvas.get_tk_widget()
 plot.place(x=0, y=0, width=CV_WIDE, height=CV_HEIGHT)
 build_empty_figure()
@@ -415,89 +425,85 @@ y_history.append(copy.deepcopy(y_all))
 canvas.draw()
 
 # Figure center
-figure_c = Label(win, text="Центр фигуры: (%3.2f;%3.2f)" % (x_all[0][0], y_all[0][0]), font=("Arial", 14), width=36,
+figure_c = Label(root, text="Центр фигуры: (%3.2f;%3.2f)" % (x_all[0][0], y_all[0][0]), font=("Arial", 14), width=36,
                  bg=TEXT_COLOR)
 figure_c.place(x=CV_WIDE + 1, y=850)
 
 # Center
-center_label = Label(win, text="Центр (для масштабирования и поворота)", font=("Arial", 14), bg=WIN_COLOR)
+center_label = Label(root, text="Центр (для масштабирования и поворота)", font=("Arial", 14), bg=WIN_COLOR)
 center_label.place(x=CV_WIDE + 15, y=20)
 
-center_x_label = Label(win, text="X:", font=("Arial", 14), bg=WIN_COLOR)
+center_x_label = Label(root, text="X:", font=("Arial", 14), bg=WIN_COLOR)
 center_x_label.place(x=CV_WIDE + 70, y=50)
-center_x = Entry(win, font=("Arial", 14), width=9)
+center_x = Entry(root, font=("Arial", 14), width=9)
 center_x.insert(END, "0")
 center_x.place(x=CV_WIDE + 100, y=50)
 
-center_y_label = Label(win, text="Y:", font=("Arial", 14), bg=WIN_COLOR)
+center_y_label = Label(root, text="Y:", font=("Arial", 14), bg=WIN_COLOR)
 center_y_label.place(x=CV_WIDE + 270, y=50)
-center_y = Entry(win, font=("Arial", 14), width=9)
+center_y = Entry(root, font=("Arial", 14), width=9)
 center_y.insert(END, "0")
 center_y.place(x=CV_WIDE + 300, y=50)
 
 # Spin
-spin_label = Label(win, text="Поворот", width=36, font=("Arial", 18), bg=TEXT_COLOR)
+spin_label = Label(root, text="Поворот", width=36, font=("Arial", 18), bg=TEXT_COLOR)
 spin_label.place(x=CV_WIDE + 1, y=110)
 
-spin_angle_label = Label(win, text="Угол°: ", font=("Arial", 14), bg=WIN_COLOR)
+spin_angle_label = Label(root, text="Угол°: ", font=("Arial", 14), bg=WIN_COLOR)
 spin_angle_label.place(x=CV_WIDE + 160, y=155)
-spin_angle = Entry(win, font=("Arial", 12), width=9)
+spin_angle = Entry(root, font=("Arial", 12), width=9)
 spin_angle.insert(END, "0")
 spin_angle.place(x=CV_WIDE + 240, y=155)
 
-spin_btn = Button(win, text="Повернуть", font=("Arial", 14), command=parse_spin, width=15,
-                  height=2, bg=TEXT_COLOR)
+spin_btn = Button(root, text="Повернуть", font=("Arial", 14), command=parse_spin, width=15, height=2, bg=TEXT_COLOR)
 spin_btn.place(x=CV_WIDE + 160, y=200)
 
 # Scale
-scale_label = Label(win, text="Масштабирование", width=36, font=("Arial", 18), bg=TEXT_COLOR)
+scale_label = Label(root, text="Масштабирование", width=36, font=("Arial", 18), bg=TEXT_COLOR)
 scale_label.place(x=CV_WIDE + 1, y=300)
 
-scale_x_label = Label(win, text="kx: ", font=("Arial", 14), bg=WIN_COLOR)
+scale_x_label = Label(root, text="kx: ", font=("Arial", 14), bg=WIN_COLOR)
 scale_x_label.place(x=CV_WIDE + 100, y=360)
-scale_x = Entry(win, font=("Arial", 14), width=9)
+scale_x = Entry(root, font=("Arial", 14), width=9)
 scale_x.insert(END, "1")
 scale_x.place(x=CV_WIDE + 140, y=360)
 
-scale_y_label = Label(win, text="ky: ", font=("Arial", 14), bg=WIN_COLOR)
+scale_y_label = Label(root, text="ky: ", font=("Arial", 14), bg=WIN_COLOR)
 scale_y_label.place(x=CV_WIDE + 270, y=360)
-scale_y = Entry(win, font=("Arial", 14), width=9)
+scale_y = Entry(root, font=("Arial", 14), width=9)
 scale_y.insert(END, "1")
 scale_y.place(x=CV_WIDE + 310, y=360)
 
-scale_btn = Button(win, text="Масштабировать", font=("Arial", 14), command=parse_scale,
+scale_btn = Button(root, text="Масштабировать", font=("Arial", 14), command=parse_scale,
                    width=15, height=2, bg=TEXT_COLOR)
 scale_btn.place(x=CV_WIDE + 160, y=420)
 
 # Move
-move_label = Label(win, text="Перемещение", width=36, font=("Arial", 18), bg=TEXT_COLOR)
+move_label = Label(root, text="Перемещение", width=36, font=("Arial", 18), bg=TEXT_COLOR)
 move_label.place(x=CV_WIDE + 1, y=520)
 
-move_x_label = Label(win, text="dx: ", font=("Arial", 14), bg=WIN_COLOR)
+move_x_label = Label(root, text="Δx: ", font=("Arial", 14), bg=WIN_COLOR)
 move_x_label.place(x=CV_WIDE + 100, y=580)
-move_x = Entry(win, font=("Arial", 14), width=9)
+move_x = Entry(root, font=("Arial", 14), width=9)
 move_x.insert(END, "0")
 move_x.place(x=CV_WIDE + 140, y=580)
 
-move_y_label = Label(win, text="dy: ", font=("Arial", 14), bg=WIN_COLOR)
+move_y_label = Label(root, text="Δy: ", font=("Arial", 14), bg=WIN_COLOR)
 move_y_label.place(x=CV_WIDE + 270, y=580)
-move_y = Entry(win, font=("Arial", 14), width=9)
+move_y = Entry(root, font=("Arial", 14), width=9)
 move_y.insert(END, "0")
 move_y.place(x=CV_WIDE + 310, y=580)
 
-move_btn = Button(win, text="Передвинуть", font=("Arial", 14), command=parse_move,
-                  width=15, height=2, bg=TEXT_COLOR)
+move_btn = Button(root, text="Переместить", font=("Arial", 14), command=parse_move, width=15, height=2, bg=TEXT_COLOR)
 move_btn.place(x=CV_WIDE + 160, y=640)
 
-line = Label(win, text="", width=36, font=("Arial", 18), bg=TEXT_COLOR)
+line = Label(root, text="", width=36, font=("Arial", 18), bg=TEXT_COLOR)
 line.place(x=CV_WIDE + 1, y=710)
 
-stab_back = Button(win, text="Шаг назад", font=("Arial", 14), command=step_backing,
-                   width=15, height=2, bg=TEXT_COLOR)
-stab_back.place(x=CV_WIDE + 25, y=760)
+step_back = Button(root, text="Шаг назад", font=("Arial", 14), command=step_backing, width=15, height=2, bg=TEXT_COLOR)
+step_back.place(x=CV_WIDE + 25, y=760)
 
-clear = Button(win, text="Сбросить", font=("Arial", 14), command=reset, width=15,
-               height=2, bg=TEXT_COLOR)
+clear = Button(root, text="Сбросить", font=("Arial", 14), command=reset, width=15, height=2, bg=TEXT_COLOR)
 clear.place(x=CV_WIDE + 300, y=760)
 
-win.mainloop()
+root.mainloop()
